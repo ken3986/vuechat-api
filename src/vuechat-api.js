224f946e3a8440ea93b6e0238cc3de21916bcb09
ken3986 <naka_ken8686@yahoo.co.jp>
2021-04-14 11:01:02 +0900

    corsのorigin:trueを外してみた

--- src/vuechat-api.js
+++ src/vuechat-api.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 
 // CORS許可
-const cors = require('cors')({origin: true});
+const cors = require('cors');
 app.use(cors);
 
 // serverless-http導入
